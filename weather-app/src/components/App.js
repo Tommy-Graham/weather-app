cbc64948d15d3ccd86c72e706bb76ce97ec2afb5
Tommy-Graham <134443039+Tommy-Graham@users.noreply.github.com>
2023-10-04 16:06:39 +0100

    Added forecasts & tests

---
+++ weather-app/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/App.css';
+import LocationDetails from './LocationDetails';
+import ForecastSummaries from './ForecastSummaries';
+
+function App({location, forecasts}) {
+  return (
+    <div className="weather-app">
+      <LocationDetails
+        city={location.city}
+        country={location.country}
+      />
+
+      <ForecastSummaries forecasts={forecasts} />
+    </div>
+  );
+};
+
+export default App;
